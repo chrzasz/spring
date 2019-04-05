3e5f7a61cc4f8f224370865a84146a92b87d768e
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-04-05 05:20:28 +0200

    first commit

---
+++ src/main/java/pl/sda/demospring/DemospringApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.demospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringApplication.class, args);
+	}
+
+}
