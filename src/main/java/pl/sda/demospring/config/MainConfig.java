d9733ea1b3587f4b03a327e07ad252d7ed8c9d26
chrzasz <chrzaszczyk@gmail.com>
2019-04-05 11:23:39 +0200

    bean via Class fix

--- src/main/java/pl/sda/demospring/config/MainConfig.java
+++ src/main/java/pl/sda/demospring/config/MainConfig.java
@@ -1,14 +1,20 @@
 package pl.sda.demospring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import pl.sda.demospring.domain.Castle;
 import pl.sda.demospring.domain.Quest;
 import pl.sda.demospring.domain.Worrior;
 
 @Configuration
-@ImportResource("classpath:config/castle-config.xml")
+//@ImportResource("classpath:config/castle-config.xml")
+@PropertySource("classpath:castle.properties")
 public class MainConfig {
+
+    @Value("${my.castle.name:Head Quarter}")
+    String name;
 
     @Bean
     public Quest createQuest() {
@@ -20,6 +26,13 @@ public class MainConfig {
         Worrior worrior = new Worrior("Zerg", 78);
         worrior.setQuest(createQuest());
         return worrior;
+    }
+
+    @Bean
+    public Castle castle() {
+        Castle castle = new Castle(worrior());
+        castle.setName(name);
+        return castle;
     }
 
 
