3ec2587b4c68e9edf4ce7be8b2296fc3d74b4cb0
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-04-05 05:24:21 +0200

    Hello World

---
+++ src/main/java/pl/sda/demospring/HelloWorld.java
@@ -0,0 +1,13 @@
+package pl.sda.demospring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
