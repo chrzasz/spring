b3943a65c4fe2e5e7c9a49e0f03b35136d06496b
chrzasz <chrzaszczyk@gmail.com>
2019-04-05 10:39:07 +0200

    bean via xml fix annotations

--- src/main/java/pl/sda/demospring/domain/Castle.java
+++ src/main/java/pl/sda/demospring/domain/Castle.java
@@ -1,24 +1,11 @@
 package pl.sda.demospring.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-@Component
-@PropertySource("classpath:castle.properties")
 public class Castle {
 
-    @Value("${my.castle.name:Mordor}")
     private String name;
-
 
     Worrior worrior;
 
-    @Autowired
     public Castle(Worrior worrior) {
         this.worrior = worrior;
     }
@@ -28,14 +15,16 @@ public class Castle {
         this.worrior = worrior;
     }
 
-    @PostConstruct
     public void build() {
         System.out.println(name + " has been build");
     }
 
-    @PreDestroy
     public void destroy() {
         System.out.println(name + " is to be destroyed");
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
