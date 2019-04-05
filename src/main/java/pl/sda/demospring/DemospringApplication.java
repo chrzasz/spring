44de9c3158afc4ecd0c6a05320809f6ceab6b314
chrzasz <chrzaszczyk@gmail.com>
2019-04-05 11:16:16 +0200

    bean via Class @Configuration

--- src/main/java/pl/sda/demospring/DemospringApplication.java
+++ src/main/java/pl/sda/demospring/DemospringApplication.java
@@ -2,10 +2,8 @@ package pl.sda.demospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:config\\spring-config.xml")
 public class DemospringApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/pl/sda/demospring/config/MainConfig.java
@@ -0,0 +1,26 @@
+package pl.sda.demospring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import pl.sda.demospring.domain.Quest;
+import pl.sda.demospring.domain.Worrior;
+
+@Configuration
+@ImportResource("classpath:config/castle-config.xml")
+public class MainConfig {
+
+    @Bean
+    public Quest createQuest() {
+        return new Quest();
+    }
+
+    @Bean
+    public Worrior worrior() {
+        Worrior worrior = new Worrior("Zerg", 78);
+        worrior.setQuest(createQuest());
+        return worrior;
+    }
+
+
+}
