68ee428881f9d113cea09531bda12032c8efe946
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-04-06 05:31:15 +0200

    service

---
+++ src/main/java/pl/sda/demospring/services/QuestService.java
@@ -0,0 +1,30 @@
+package pl.sda.demospring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.sda.demospring.domain.Quest;
+import pl.sda.demospring.domain.repository.QuestRepository;
+import pl.sda.demospring.domain.repository.WorriorRepository;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class QuestService {
+
+    @Autowired
+    WorriorRepository worriorRepository;
+
+    @Autowired
+    QuestRepository questRepository;
+
+    final static Random rand = new Random();
+
+
+    public void assignRandomQuest(String worriorName) {
+        List<Quest> allQuests = questRepository.getAll();
+        Quest randomQuest = allQuests.get(rand.nextInt(allQuests.size()));
+        worriorRepository.getWorrior(worriorName).setQuest(randomQuest);
+    }
+
+}
