affc2cf121526471dfcad19bf7abc97dcd13f384
Greg <chrzaszczyk@gmail.com>
2019-04-14 14:32:32 +0200

    adding attribude from backend to frontend

--- src/main/java/pl/sda/demospring/mvccontrollers/UserController.java
+++ src/main/java/pl/sda/demospring/mvccontrollers/UserController.java
@@ -1,12 +1,14 @@
 package pl.sda.demospring.mvccontrollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/user")
-    public String user8746131331() {
+    public String user8746131331(Model model) {
+        model.addAttribute("atr", "1234"); //1234-wartosc przekazywana na frontend
         return "user"; //this is html file name
     }
 }
